29fefe416587196f31725bf8c14ec733293518ee
hanmok <dmammmm@naver.com>
2023-04-08 20:24:54 +0900

    Add gitignore

--- Util/build/Util.build/Debug-iphoneos/Util.build/DerivedSources/Util_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char UtilVersionString[];
- extern const double UtilVersionNumber;
-
- const unsigned char UtilVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Util  PROJECT:Util-1" "\n";
- const double UtilVersionNumber __attribute__ ((used)) = (double)1.;
